a574319f118b0e40bcf58e332212b58907617f38
ShadyART <mariusgeorgem@yahoo.com>
2018-02-04 18:49:48 +0200

    Progress

--- private/js/Factionlist.js
+++ private/js/Factionlist.js
@@ -6,19 +6,7 @@ module.exports = class Factionlist extends List{
     super();
     this.lastid = 0;
   }
-  remove(varname, varval, onlyfirst = 1){
-    var found = 0;
-    for(var i = 0; i < this.length; ++i){
-        if(this[i][varname] == varval){
-            this.splice(i, 1);
-            if(onlyfirst)
-                return 1;
-            found = 1;
-            --i;
-        }
-    }
-    return found;
-  }
+    
     insert(faction){
         faction.setId(this.lastid);
         this.lastid++;
--- private/js/List.js
+++ private/js/List.js
@@ -7,4 +7,18 @@ module.exports = class List extends Array{
       el.save();
     });
   }
+
+    remove(varname, varval, onlyfirst = 1){
+        var found = 0;
+        for(var i = 0; i < this.length; ++i){
+            if(this[i][varname] == varval){
+                this.splice(i, 1);
+                if(onlyfirst)
+                    return 1;
+                found = 1;
+                --i;
+            }
+        }
+        return found;
+    }
 }
